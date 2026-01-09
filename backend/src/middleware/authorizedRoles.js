42aa589ae7fa7d66034046c75fd720800da70996
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-01-09 09:56:28 -0300

    fix
    Concertando a parte de permissões

--- backend/src/middleware/authorizedRoles.js
+++ backend/src/middleware/authorizedRoles.js
@@ -3,6 +3,9 @@ export function AuthorizeRoles(...allowedRoles) {
     // recupera as informacoes do user no "req" (vem do authorization)
     const { role } = req.user;
 
+    console.log(role);
+    console.log(`...\n ${allowedRoles}`)
+
     if (!allowedRoles.includes(role)) {
       return res.status(403).json({
         error: "Você não tem permissão para acessar este recurso.",
