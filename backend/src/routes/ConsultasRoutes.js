1410c0ef4c998ef7352d3c5a5448441d518542ce
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-01-28 11:38:32 -0300

    feat
    criando rota para pesquisa de consultas com paginação e pesquisa

--- backend/src/routes/ConsultasRoutes.js
+++ backend/src/routes/ConsultasRoutes.js
@@ -7,6 +7,7 @@ import ConsultasCLTController from "../controllers/ConsultasCLTController.js";
 
 const ConsultasRoutes = express.Router();
 
+ConsultasRoutes.get("/FGTS/manual", AuthenticationToken, ConsultasFGTSController.RecuperarConsultas);
 ConsultasRoutes.post("/FGTS/manual", AuthenticationToken, ConsultasFGTSController.FazerConsulta);
 ConsultasRoutes.post("/CLT/manual", AuthenticationToken, ConsultasCLTController.FazerConsulta);
 
