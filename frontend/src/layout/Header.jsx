1df1b3e840734b929aeba3b9100db919dc16e76f
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-02-03 09:57:17 -0300

    test
    enviando um commit para testar o deploy

--- frontend/src/layout/Header.jsx
+++ frontend/src/layout/Header.jsx
@@ -3,7 +3,7 @@ import "./header.css";
 export default function Header() {
   return (
     <header className="app-header">
-      <h1>CRM OMNI TECH AI</h1>
+      <h1>CRM OMNI TECH AI TESTE</h1>
     </header>
   );
 }
