fafc90e9a0890c91e66b0ed6c1b0430b0c9e230c
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-02-13 14:34:01 -0300

    feat
    adicionando verificação na estrura do csv que chega no ms-python

--- python_ms/app/services/consulta_lote_service.py
+++ python_ms/app/services/consulta_lote_service.py
@@ -11,5 +11,16 @@ class consulta_lote_service:
     def inciar_consulta_lote(self):
         csv_df = csv_repository(self.local_path).getDataFrame()
 
-        print(csv_df)
-        print(self.instituicao)
+        # Verificação da estrutura do arquivo (colunas):
+        colunasValidas = {
+            "CPF",
+            "Cliente",
+            "Dt Nasc",
+            "Celular"
+        }
+        colunas_df = set(csv_df.columns)
+        if (colunas_df != colunasValidas):
+            print("COLUNAS ERRADAS, RETORNAR ERRO.")
+        else:
+            print("Colunas certas.")
+
