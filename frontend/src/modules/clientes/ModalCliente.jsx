eb7c6d009a48f36223bb3bdcde7d017030106c9a
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-03-03 14:43:56 -0300

    feat
    terminando a lógica de recuperar dados dos cliente

--- frontend/src/modules/clientes/ModalCliente.jsx
+++ frontend/src/modules/clientes/ModalCliente.jsx
@@ -4,33 +4,59 @@ import './modalCliente.css';
 
 export default function ModalCliente({cpf}) {
     const [ loading, setLoading ] = useState(false);
-    const [ clienteData, setClienteData ] = useState([]);
+    const [ clienteData, setClienteData ] = useState(null);
+    const [ errorRequest, setErrorRequest ] = useState(null);
 
-    const buscarDadosCliente = async () => {
+    const buscarDadosCliente = async (propsCpf) => {
         try {
             setLoading(true);
 
             const response = await api.get(
-                `/clientes?cpf=${cpf}`
+                `/clientes?cpf=${propsCpf}`
             );
 
-            setClienteData(response.data.data || []);
+            setClienteData(response.data || []);
         } catch (error) {
-            console.error("Erro ao dados do cliente:", error);
+            setClienteData(null);
+
+            if (error.response.status == 424) {
+                setErrorRequest(error.response.data.erro)
+            } else {
+                console.error(`Erro não tratado na hora de recuperar dados do cliente: ${error}`)
+            }
         } finally {
             setLoading(false);
         }
     };
 
     useEffect(() => {
-        buscarDadosCliente();
-    }, [])
+        if (!cpf) return;
+
+        buscarDadosCliente(cpf);
+    }, [cpf]);
 
     return (
         <div className="info-cliente-container">
-            {loading ? "Carregando" : (
-                <p>{clienteData}</p>
+            {loading ? "Carregando..." : clienteData && (
+                <div>
+                    <h3>Informações do cliente</h3>
+                    <br />
+                    <p><b>Nome:</b> {clienteData.nome}</p>
+                    <p><b>Cpf:</b> {clienteData.cpf}</p>
+                    <p><b>Data de nascimento:</b> {clienteData.data_nasc}</p>
+                    <p><b>Celular:</b> {clienteData.celular}</p>
+                </div>
             )}
+
+            {errorRequest ? (
+                <div>
+                    <h3>Informações do cliente</h3>
+                    <br />
+                    <p><b>Erro ao consultar os dados do cliente:</b></p>
+                    <br />
+                    <p>{errorRequest}</p>
+                </div>
+            ): ""}
         </div>
     )
 }
