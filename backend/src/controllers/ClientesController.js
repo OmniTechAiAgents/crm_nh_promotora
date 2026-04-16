4c5cb6bf2062e861eaf49e34320ca07a2bc593cc
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-04-16 12:01:39 -0300

    feat
    criando a lógica principal da branch, verificar cliente existente no DB, se não existir busca no nova vida

--- backend/src/controllers/ClientesController.js
+++ backend/src/controllers/ClientesController.js
@@ -71,6 +71,39 @@ class ClientesController {
             return res.status(500).json({ erro: err.message });
         }
     }
+
+    async BuscarClienteDBeConsultarNovaVida(req, res) {
+        try {
+            const { cpf } = req.body;
+
+            const resultBuscaCliente = await ClientesService.procurarCpf(cpf); 
+            if (!resultBuscaCliente || resultBuscaCliente?.length === 0) {
+                const dadosCliente = await NovaVidaService.BuscarDados(cpf);
+            
+                if(dadosCliente.CONSULTA == "Não Autorizado") {
+                    throw new HttpException("Não foi possível recuperar os dados do cliente na API do Nova Vida, será necessário fazer o cadastro do cliente manualmente.", 424);
+                }
+
+                await ClientesService.criarClienteNovaVida(dadosCliente, cpf);
+            }
+            
+            const cliente = await ClientesService.procurarCpf(cpf);
+
+            return res.status(200).json(cliente.dataValues);
+        } catch(err) {
+            if (err instanceof ZodError) {
+                return res.status(400).json({
+                    erro: err.issues[0].message
+                });
+            }
+
+            if (err instanceof HttpException) {
+                return res.status(err.status).json({ erro: err.message });
+            }
+
+            return res.status(500).json({ erro: err.message });
+        }
+    }
 }
 
 export default new ClientesController();
