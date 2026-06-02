49200d6d5a88d3eb358e03c2a8d13c2c8eb56567
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-06-02 13:11:30 -0300

    chore/fix
    recolocando, arrumando e melhorando as alterações do rollback em relação aos containers

--- backend/src/app.js
+++ backend/src/app.js
@@ -71,7 +71,7 @@ async function bootstrap() {
         console.log("✅ Token C6 carregado e agendamento ativo");
 
         const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
+        app.listen(PORT, "0.0.0.0", () => {
             console.log(`🚀 Servidor rodando na porta ${PORT}`);
         });
 
