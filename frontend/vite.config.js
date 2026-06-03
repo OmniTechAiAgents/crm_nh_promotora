a74bd5fa02600c863b9792e218483ff32886dd76
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-06-03 14:59:19 -0300

    feat
    separando ambiente de producao do de desenvolvimento

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['newsale.tech'],
+    cors: {
+      origin: ['http://newsale.tech', 'https://newsale.tech'],
+      credentials: true
+    }
+  }
 })
